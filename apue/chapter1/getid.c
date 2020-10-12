10d057c37b2e18a2c748fbf09caec45c4bff6934
dmaner <2663515256@qq.com>
2020-10-12 18:28:46 +0800

    re ignore

--- apue/chapter1/getid.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main()
-{
-    printf("uid = %d, gid = %d\n", getuid(), getgid());
-    return 0;
-}
--- apue/chapter1/printpid.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main()
-{
-    printf("Hello world from process ID :%ld\n", (long)getpid());
-}
